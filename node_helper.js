f080a1d47efc5b6c2c27522ae8b952e1ff07a722
idoodler <me@idoodler.de>
2019-11-17 21:13:49 +0100

    Fixed logger

--- node_helper.js
+++ node_helper.js
@@ -2,7 +2,7 @@ var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
     start: function start() {
-        Log.info(this.name, "Starting environmental variable injection");
+        console.log("MMM-ENV-Provider", "Starting environmental variable injection");
         this.expressApp.get("/env", function (req, res) {
             res.send(process.env);
         }.bind(this));
