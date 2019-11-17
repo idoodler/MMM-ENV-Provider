910fa6873d6f7e728cefd7bade56799a810cf99c
idoodler <me@idoodler.de>
2019-11-17 20:24:43 +0100

    Added logger

--- node_helper.js
+++ node_helper.js
@@ -2,6 +2,7 @@ var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
     start: function start() {
+        Log.info(this.name, "Starting environmental variable injection");
         this.expressApp.get("/env", function (req, res) {
             res.send(process.env);
         }.bind(this));
